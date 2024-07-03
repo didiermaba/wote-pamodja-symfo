70c8e1099377e1136d961a4a06c070b10061268e
Didier Maba <deus_maba@yahoo.fr>
2024-07-03 06:19:37 +0200

    m. pl mailpit & contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\ContactDTO;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\mailer;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $data = new ContactDTO();
+
+        $form = $this->createForm(ContactType::class, $data);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mail = (new TemplatedEmail())
+                ->to($data->service)
+                ->from($data->email)
+                ->subject('Demande de contact')
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context(['data' => $data]);
+            try {
+                $mailer->send($mail);
+                $this->addFlash('success', 'Votre email a bien été envoyé');
+                return $this->redirectToRoute('contact');
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'Impossible d\'envoyer votre email');
+            }
+        }
+        return $this->render('category/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
