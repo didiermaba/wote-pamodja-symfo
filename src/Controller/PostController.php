2e20dd9a710ed276754d0be84246c4f29e008ddd
Didier Maba <deus_maba@yahoo.fr>
2024-07-04 04:39:51 +0200

    récup des données dynamiques Post & Comments

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
-    {
+    #[Route('/post', name: 'post',  methods:['GET'])]
+    public function index(
+        PostRepository $postRepository,
+        CategoryRepository $categoryRepository
+
+    ): Response {   
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $postRepository->findBy(
+                [],
+                ['title' => 'ASC'], // ordre ascendant par titre 
+                10
+            ),
+
+            'categories' => $categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/{category}', name: 'category', methods:['GET'])]
+    public function category(
+        Request $request, 
+        CategoryRepository $categoryRepository,
+        PostRepository $postRepository, 
+    ): Response {   
+        $category = $categoryRepository->findOneBy([
+            'name' => $request->get('category')
+        ]);
+
+        return $this->render('post/index.html.twig', [
+            'categories' => $category,
+            'posts' => $postRepository->findBy(['category' => $category])
         ]);
     }
 }
